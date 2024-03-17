46c6ce5079648f7748d07f8abb45b1fed6f7e0cb
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2024-03-17 17:08:14 +0000

    I think project should be runnable now by anyone + need to refine a little bit grammar and code generation

---
+++ runtime-EclipseApplication/XPectTests/src/uk/ac/kcl/XPectTests.java
@@ -0,0 +1,9 @@
+package uk.ac.kcl;
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
+import org.junit.runner.RunWith;
+
+@RunWith(XpectRunner.class)
+public class XPectTests extends XtextTests {
+
+}
